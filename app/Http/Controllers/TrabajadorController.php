88dd1cc62928a744dadafd3d9dac6f0e0dc54233
mirandevel <miranda.devel@gmail.com>
2021-02-25 16:13:57 -0400

    Trabajador controller

--- app/Http/Controllers/TrabajadorController.php
+++ app/Http/Controllers/TrabajadorController.php
@@ -9,23 +9,14 @@ use Illuminate\Http\Request;
 class TrabajadorController extends Controller
 {
     public function obtenerTrabajadores(Request $request){
-        return Trabajador::select('trabajadores.*','personas.nombre','personas.ci','users.email')
+        $trabajador=Trabajador::select('trabajadores.*','personas.nombre','personas.ci','users.email')
             ->join('personas','personas.id','=','trabajadores.persona_id')
-            ->join('users','users.persona_id','=','personas.id')
-            ->get();
-        /*
-         public function crearServicioTrabajador($trabajador_id, $item){
-        ServicioTrabajador::create([
-            'fecha' => Carbon::now('America/La_Paz')->toDateString(),
-            'dias' => $item["dias"],//lun,mar,mie,jue
-            'hora_inicio' => $item["hora_inicio"],
-            'hora_fin' => $item["hora_fin"],
-
-            'trabajador_id' => $trabajador_id,
-            'servicio_id' => $item["servicio_id"],
-        ]);
-    }
-         */
+            ->join('users','users.persona_id','=','personas.id');
+        if($request['estado']!='t'){
+         $trabajador=$trabajador->where('habilitado',$request['estado']);
+        }
+            $trabajador=$trabajador->get();
+        return $trabajador;
     }
 
     public function aceptarTrabajadores(Request $request){
