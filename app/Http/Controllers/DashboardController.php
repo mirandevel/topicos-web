1616b45dbbea8bb6cdd61678cacb33b0c20ebf15
mirandevel <miranda.devel@gmail.com>
2021-03-16 10:45:45 -0400

    Dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Solicitud;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
 public function dashboard(){
-    $users = User::
-    select(User::raw('DATE(created_at) as date'), User::raw('count(*) as cantidad'))
+        $datos['usuarios']=$this->usuarios();
+        $datos['solicitudes']=$this->solicitudes();
+}
+
+public function usuarios(){
+    $users = User::select(User::raw('DATE(created_at) as date'), User::raw('count(*) as cantidad'))
         ->groupBy('date')
         ->orderBy('date','asc')
         ->get();
+
     $i = 0;
     $fechas=[];
     $cantidades=[];
@@ -27,4 +33,25 @@ public function dashboard(){
 
     return $datos;
 }
+
+    public function solicitudes(){
+        $users = Solicitud::select(Solicitud::raw('DATE(created_at) as date'), Solicitud::raw('count(*) as cantidad'))
+            ->groupBy('date')
+            ->orderBy('date','asc')
+            ->get();
+
+        $i = 0;
+        $fechas=[];
+        $cantidades=[];
+        foreach ($users as $user) {
+            $fechas[$i] = $user->date;
+            $cantidades[$i] = $user->cantidad;
+            $i = $i + 1;
+        }
+        $datos['fechas']=$fechas;
+        $datos['cantidades']=$cantidades;
+        $datos['total_solicitudes']=Solicitud::count();
+
+        return $datos;
+    }
 }
